85120850e16fd387653bc20d82342c546802a183
Adam Essmyer <adamessmyer@Adams-MacBook-Pro.local>
2022-09-28 08:38:06 -0500

    created menu, imported store image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold">Welcome to Lakeside!</h1>
+      <Navbar />
     </div>
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Image from '../assets/LGfront.jpeg'
 
 const Navbar = () => {
     return(
-        <div className='fixed w-full h-[80px] flex: justify-between'>
+        <div className='fixed w-full h-[80px] flex: justify-between items-center px-4 bg-black text-gray' >
+            <div>
+                <img src={Image} alt="lakeside" />
+
+                {/*Menu is here*/}
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Products</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+
+            {/*Hamburger*/}
+            <div>
+                
+            </div>
+
 
         </div>
     )
