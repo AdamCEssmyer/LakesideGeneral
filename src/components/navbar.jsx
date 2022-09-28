3980d00722883a76399bd0347523128805cf200b
Adam Essmyer <adamessmyer@Adams-MacBook-Pro.local>
2022-09-28 07:11:25 -0500

    started work on navbar return

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
     return(
-        <div>Navbar</div>
+        <div className='fixed w-full h-[80px] flex: justify-between'>
+
+        </div>
     )
 }
 
